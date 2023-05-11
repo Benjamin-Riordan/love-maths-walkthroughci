e1b3700964a80c08abb72a4db336d4a60b87af03
Benjamin-Riordan <>
2023-05-11 13:25:41 +0000

    final addtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,8 @@ function runGame(gameType){
     displayMultiplyQuestion(num1, num2)
    }else if (gameType === "subtract"){
     displaySubtractQuestion(num1, num2)
+   }else if (gameType === "division"){
+    displayDivisionQuestion((num1 * num2),num2)
    }else{
     alert(`Unknown game type : ${gameType}`);
     throw `Unknown game type : ${gameType}.Aborting`
@@ -74,7 +76,10 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2 ,"multiply"]
     }else if(operator === "-"){
         return[operand1 - operand2 , "subtract"]
-    }else{
+    }else if(operator === "/"){
+        return[operand1 / operand2 , "division"]
+    }
+    else{
         alert(`Unimplented operator ${operator}`);
         throw `Unimplented operator ${operator}.Aborting1`
     }
@@ -114,6 +119,14 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById("operand1").textContent =operand1
     document.getElementById("operand2").textContent =operand2
     document.getElementById("operator").textContent = "x"
+   
+    
+}
+
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent =operand1 > operand2 ? operand1 : operand2
+    document.getElementById("operand2").textContent =operand1 > operand2 ? operand2 : operand1
+    document.getElementById("operator").textContent = "/"
    
     
 }
